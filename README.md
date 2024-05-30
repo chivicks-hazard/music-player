a67d9fc754046523069bc6a955a801b066a6ba20
Victor Chigbo <chigbovcm2020@gmail.com>
2024-05-30 05:25:48 +0100

    Music Player v2.0.0. More info in the README.md files

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # My Music Player
 
 This is a Web-based music player which uses vanilla CSS and JavaScript with Font Awesome for icons
+
+## The First Version
+![The first version](doc-pics/v1.PNG)
+
+## The Second Version (First Design Overhaul)
+![The second version](doc-pics/v2.PNG)
--- script/music.js
+++ script/music.js
@@ -1,77 +1,147 @@
-/* 
-	Some music files to be played
-
-	file: name of the file
-	name: title of the song
-	album_pic: album cover of the song
- */
-var music = [
+const music = [
 		{
-			file: 'Aye.mp3',
-			name: 'Aye',
-			album_pic: 'desert.jpeg'
+			file: 'Burna_Boy_-_Last_Last.mp3',
+			name: 'Last Last',
+			artist: 'Burna Boy',
+			album: 'Love, Damini',
+			album_cover: 'burna_boy_-_love,_damini.png'
 		},
 		{
-			file: 'Asa_Jailer.mp3',
-			name: 'Jailer',
-			album_pic: ''
+			file: 'Burna_Boy_-_Its_Plenty.mp3',
+			name: 'Its Plenty',
+			artist: 'Burna Boy',
+			album: 'Love, Damini',
+			album_cover: 'burna_boy_-_love,_damini.png'
 		},
 		{
-			file: 'Bring It On.mp3',
-			name: 'Bring It On',
-			album_pic: 'desert3.jpeg'
+			file: 'Burna_Boy_-_Common_Person.mp3',
+			name: 'Common Person',
+			artist: 'Burna Boy',
+			album: 'Love, Damini',
+			album_cover: 'burna_boy_-_love,_damini.png'
 		},
 		{
-			file: 'Burna_Boy_Ye.mp3',
-			name: 'Ye',
-			album_pic: 'forest.jpeg'
+			file: 'Sia_-_Fist_Fighting_A_Sandstorm.mp3',
+			name: 'Fist Fighting A Sandstorm',
+			artist: 'Sia',
+			album: 'This Is Acting',
+			album_cover: 'sia_-_this_is_acting.png'
 		},
 		{
-			file: 'Coldplay_-_Paradise.mp3',
-			name: 'Paradise',
-			album_pic: 'grass.jpeg'
+			file: 'Sia_-_Reaper.mp3',
+			name: 'Reaper',
+			artist: 'Sia',
+			album: 'This Is Acting',
+			album_cover: 'sia_-_this_is_acting.png'
+		},
+		{
+			file: 'Sia_-_Space_Between.mp3',
+			name: 'Space Between',
+			artist: 'Sia',
+			album: 'This Is Acting',
+			album_cover: 'sia_-_this_is_acting.png'
+		},
+		{
+			file: 'Juice_WRLD_-_Wishing_Well.mp3',
+			name: 'Wishing Well',
+			artist: 'Juice WRLD',
+			album: 'Legends Never Die',
+			album_cover: 'legends_never_die_-_juice_wrld.jpg'
+		},
+		{
+			file: 'Juice_WRLD_ft_Marshmello_-_Come_And_Go.mp3',
+			name: 'Come And Go',
+			artist: 'Juice WRLD ft. Marshmello',
+			album: 'Legends Never Die',
+			album_cover: 'legends_never_die_-_juice_wrld.jpg'
+		},
+		{
+			file: 'Westlife_-_Soledad.mp3',
+			name: 'Soledad',
+			artist: 'Westlife',
+			album: 'Coast To Coast',
+			album_cover: 'coast_to_coast_-_westlife.jpg'
+		},
+		{
+			file: 'Westlife_-_I_Lay_My_Love_On_You.mp3',
+			name: 'I Lay My Love On You',
+			artist: 'Westlife',
+			album: 'Coast To Coast',
+			album_cover: 'coast_to_coast_-_westlife.jpg'
+		},
+		{
+			file: 'Westlife_-_My_Love.mp3',
+			name: 'My Love',
+			artist: 'Westlife',
+			album: 'Coast To Coast',
+			album_cover: 'coast_to_coast_-_westlife.jpg'
+		},
+		{
+			file: 'Ella_Henderson_-_Friends.mp3',
+			name: 'Friends',
+			artist: 'Ella Henderson',
+			album: 'Glorious',
+			album_cover: 'ella_henderson_-_glorious.jpg'
+		},
+		{
+			file: 'Allen_Stone_-_Consider_Me.mp3',
+			name: 'Consider Me',
+			artist: 'Allen Stone',
+			album: 'Building Balance',
+			album_cover: 'allen_stone_-_building_balance.jpg'
 		}
 ];
 
+var musicPlayer = document.getElementById('MP');
+var play_n_pause_btn = document.querySelector('.playPause');
+var albumPic = document.getElementById('albumCover');
+var noAlbumPic = document.querySelector('.fa-music');
+var next_btn = document.querySelector('.fa-step-forward');
+var prev_btn = document.querySelector('.fa-step-backward');
+var songDetails = document.querySelector('.song-details');
+var progressArea = document.querySelector('.mpProgress');
+var progressBar = document.querySelector('.mpProgressBar');
+var current_play_time = document.querySelector('.current-time');
+var songTime = document.querySelector('.max-time');
 var index = 0;
 
-/* music.html should be agood reference for this */
-var play_pause_btn = document.querySelector('.playPause');
-var prev_btn = document.querySelector('.fa-step-backward');
-var next_btn = document.querySelector('.fa-step-forward');
-var musicPlayer = document.getElementById('Music');
-var Check = document.getElementById('check');
-var MusicName = document.getElementById('mpName');
-var albumPic = document.querySelector('.album-pic').children[0];
-var noAlbumPic = document.querySelector('.album-pic').children[1];
-// console.log(noAlbumPic);
+// Event Listeners
+window.onload = loadMusic(index);
 
-window.onload = function() {
-	loadMusic(index);
-}
+play_n_pause_btn.addEventListener('click', PlayPause)
 
-play_pause_btn.addEventListener('click', PlayPause);
+next_btn.addEventListener('click', nextSong);
 
+prev_btn.addEventListener('click', prevSong);
+
+musicPlayer.addEventListener('timeupdate', playTime);
+
+progressArea.addEventListener('click', seekPlay);
+
+
+//Functions
 // Loading the 'CDs' from the music array of objects 😉 
 function loadMusic(musicIndex) {
 	// body...
 	musicPlayer.src = `media/music/${music[musicIndex].file}`;
-	MusicName.innerText = music[musicIndex].name;
-	if (music[musicIndex].album_pic === '') {
+	songDetails.children[0].innerText = `Name: ${music[musicIndex].name}`;
+	songDetails.children[1].innerText = `Artist: ${music[musicIndex].artist}`;
+	songDetails.children[2].innerText = `Album: ${music[musicIndex].album}`;
+	if (music[musicIndex].album_cover === '') {
 		albumPic.style.display = 'none';
 		noAlbumPic.style.display = 'block';
 	} else {
 		noAlbumPic.style.display = 'none';
 		albumPic.style.display = 'block';
-		albumPic.src = `media/pic/${music[musicIndex].album_pic}`;
+		albumPic.src = `media/pic/${music[musicIndex].album_cover}`;
 	}
 
 }
 
-// 
+// For the play and pause button
 function PlayPause() {
 
-	if (Check.innerText === 'Pause') {
+	if (play_n_pause_btn.classList.contains('fa-play') === true) {
 		playMusic();
 	} else {
 		pauseMusic();
@@ -82,24 +152,21 @@ function PlayPause() {
 // For playing
 function playMusic(argument) {
 	musicPlayer.play();
-	if (Check.innerText === 'Pause') {
-		play_pause_btn.classList.remove('fa-play');
-		play_pause_btn.classList.add('fa-pause');
-		Check.innerText = 'Playing';
+	if (play_n_pause_btn.classList.contains('fa-play') === true) {
+		play_n_pause_btn.classList.remove('fa-play');
+		play_n_pause_btn.classList.add('fa-pause');
+		
 	}
 }
 
 // For pausing, if that is a word
 function pauseMusic(argument) {
 	musicPlayer.pause();
-	if (Check.innerText === 'Playing') {
-		play_pause_btn.classList.remove('fa-pause');
-		play_pause_btn.classList.add('fa-play');
-		Check.innerText = 'Pause';
+	if (play_n_pause_btn.classList.contains('fa-pause') === true) {
+		play_n_pause_btn.classList.remove('fa-pause');
+		play_n_pause_btn.classList.add('fa-play');
 	}
 }
-
-next_btn.addEventListener('click', nextSong);
 
 // This gives the Play Next button its function
 function nextSong() {
@@ -114,14 +181,16 @@ function nextSong() {
 
 	loadMusic(index);
 
-	if (Check.innerText === 'Pause') {
+	if (play_n_pause_btn.classList.contains('fa-play') === true) {
 		pauseMusic();
 	} else {
 		playMusic();
 	}
-}
 
-prev_btn.addEventListener('click', prevSong);
+	if (musicPlayer.currentTime === 0) {
+		progressBar.style.width = '0';
+	}
+}
 
 // This gives the Play Prev button its function
 function prevSong() {
@@ -131,9 +200,59 @@ function prevSong() {
 
 	loadMusic(index);
 
-	if (Check.innerText === 'Pause') {
+	if (play_n_pause_btn.classList.contains('fa-play') === true) {
 		pauseMusic();
 	} else {
 		playMusic();
 	}
+
+	if (musicPlayer.currentTime === 0) {
+		progressBar.style.width = '0';
+	}
+}
+
+// For the progress bar and song timers
+function playTime(player) {
+	const currentPlayTime = player.target.currentTime;
+	const playDuration = player.target.duration;
+	let progressWidth = (currentPlayTime / playDuration) * 100;
+	progressBar.style.width = `${progressWidth}%`;
+
+	musicPlayer.addEventListener('loadeddata', function(playdata) {
+		let songDuration = playdata.target.duration;
+		let totalMin = Math.floor(songDuration / 60);
+		let totalSec = Math.floor(songDuration % 60);
+
+		if (totalSec < 10) {
+			totalSec = `0${totalSec}`;
+		}
+
+		songTime.innerText = `${totalMin}:${totalSec}`;
+	})
+
+	let currentMin = Math.floor(currentPlayTime / 60);
+	let currentSec = Math.floor(currentPlayTime % 60);
+
+	if (currentSec < 10) {
+		currentSec = `0${currentSec}`;
+	}
+
+	current_play_time.innerText = `${currentMin}:${currentSec}`;
+}
+
+// If the progress bar is clicked at any point
+function seekPlay(seek) {
+	let progressWidth = progressArea.clientWidth;
+	let clickedOfffsetX = seek.offsetX;
+	let songDuration = musicPlayer.duration;
+
+	// console.log(clickedOfffsetX);
+
+	musicPlayer.currentTime = (clickedOfffsetX / progressWidth) * songDuration;
+
+	if (play_n_pause_btn.classList.contains('fa-play') === true) {
+		pauseMusic();
+	}
+
+
 }
